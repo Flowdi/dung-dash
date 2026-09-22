3f6f30570f08aad84996027c928be3b03bd170e7
Florian Dumler <flodumler@gmail.com>
2026-09-22 09:09:42 +0200

    Add a compact control guide

--- tests/bundle.test.js
+++ tests/bundle.test.js
@@ -16,6 +16,8 @@ test("index loads the classic bundle for direct file usage", async () => {
   assert.match(html, /id="current-missions"/);
   assert.match(html, /id="reset-run-btn"/);
   assert.match(html, /id="level-select"/);
+  assert.match(html, /class="control-guide"/);
+  assert.match(html, /<kbd>Leertaste<\/kbd>/);
   assert.doesNotMatch(html, /id="jump-charge"/);
   assert.match(html, /id="level-menu-btn"/);
   assert.match(html, /id="next-level-btn"/);
